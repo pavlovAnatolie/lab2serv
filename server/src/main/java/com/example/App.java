90c5109984c611439bec1fb4590421d0fea5e69d
lai2-01 <lai2-01@itismeucci.com>
2023-10-09 09:48:46 +0200

    fin server

--- server/src/main/java/com/example/App.java
+++ server/src/main/java/com/example/App.java
@@ -1,5 +1,11 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try{
+           ServerSocket server = new ServerSocket(3000);
+
+           System.out.println("il server è in ascolto");
+           Socket s = server.accept();
+
+            
+           BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+           DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+           String frase = in.readLine();
+           frase = frase.toUpperCase();
+           System.out.println("li server invia: "+frase);
+           out.writeBytes(frase + "\n");
+
+            s.close();
+            server.close();
+
+        server.accept();  
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+       
+
     }
 }
